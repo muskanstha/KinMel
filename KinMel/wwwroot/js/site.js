c3f43c2d00bf91f74231e9a2984954702d5bcd9b
Muskan Shrestha <musk0047@edu.easj.dk>
2018-04-25 21:29:52 +0200

    Add Materialize

--- KinMel/wwwroot/js/site.js
+++ KinMel/wwwroot/js/site.js
@@ -1,1 +1,9 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+
+    $('.parallax').parallax();
+
+    $('.dropdown-trigger').dropdown();
+    //console.log(1);
+});
