576c9db6f4d19e00b8f506c19e1e43a518f27f83
Muskan Shrestha <musk0047@edu.easj.dk>
2018-04-26 00:28:53 +0200

    hosted db

--- KinMel/Startup.cs
+++ KinMel/Startup.cs
@@ -27,7 +27,7 @@ namespace KinMel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("HostedConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
