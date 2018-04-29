7462dc596a2ac2161770cb33f3095249a4f7cae0
Vattujan <yoge0030@edu.easj.dk>
2018-04-29 16:40:17 +0200

    Login, Register, Forgot Password, Footer fix

--- KinMel/Models/AccountViewModels/LoginViewModel.cs
+++ KinMel/Models/AccountViewModels/LoginViewModel.cs
@@ -16,7 +16,7 @@ namespace KinMel.Models.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
     }
 }
