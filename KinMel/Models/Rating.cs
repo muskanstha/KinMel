98ab2141e41f9c33488d36300cd6765357c97b01
Muskan Shrestha <musk0047@edu.easj.dk>
2018-05-03 22:57:33 +0200

    add rating class

---
+++ KinMel/Models/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinMel.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string RatedById { get; set; }
+        public ApplicationUser RatedBy { get; set; }
+
+        public string RatedForId { get; set; }
+        public ApplicationUser RatedFor { get; set; }
+
+        public int Stars { get; set; }
+        public string Review { get; set; }
+
+    }
+}
