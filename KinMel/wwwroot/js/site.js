22f575a93b74e8ed2bdcde696e4a4c08203a0c7c
Vattujan <yoge0030@edu.easj.dk>
2018-05-05 20:12:04 +0200

    sorting

--- KinMel/wwwroot/js/site.js
+++ KinMel/wwwroot/js/site.js
@@ -1,12 +1,27 @@
 ﻿// Write your JavaScript code.
 $(document).ready(function () {
     M.AutoInit();
+    //preloader
+    $(window).load(function () {
+        setTimeout(function () {
+            $("body").addClass("loaded");
+        }, 150);
+    }); 
+    // Materialize Dropdown
+    $('.dropdown-trigger').dropdown({
+        coverTrigger: false // Displays dropdown below the button
+    });
+    //Tooltip
+    $(document).ready(function () {
+        $(".tooltipped").tooltip({ delay: 50 });
+    });
     //$('.sidenav').sidenav();
 
     //$('.parallax').parallax();
 
-    //$('.dropdown-trigger').dropdown();
+    
     ////console.log(1);
     //$('select').formSelect();
+
 
 });
