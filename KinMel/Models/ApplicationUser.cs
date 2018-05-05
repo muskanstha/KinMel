15d3f95d0ee7e5eef0383582cd24b214c69f2b92
Muskan Shrestha <musk0047@edu.easj.dk>
2018-05-05 19:52:39 +0200

    user profile and edit profile: v1

--- KinMel/Models/ApplicationUser.cs
+++ KinMel/Models/ApplicationUser.cs
@@ -52,4 +52,25 @@ namespace KinMel.Models
 
         public DateTime JoinDate { get; set; }
     }
+
+    public class AccountDTO
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        
+        public virtual ICollection<ClassifiedAd> ClassifiedAds { get; set; }
+
+        public virtual ICollection<Rating> Ratings { get; set; }
+
+        public double AverageStars { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
 }
