514df40a9ff89a8265e28356e421cd62104011c5
Muskan Shrestha <musk0047@edu.easj.dk>
2018-05-12 18:59:24 +0200

    upload profile picture

--- KinMel/Models/ApplicationUser.cs
+++ KinMel/Models/ApplicationUser.cs
@@ -13,7 +13,22 @@ namespace KinMel.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public string ProfilePictureUrl { get; set; }
+        private string _profilePictureUrl;
+
+        public string ProfilePictureUrl
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(this._profilePictureUrl))
+                {
+                    return "/images/noimage.svg";
+                }
+
+                return this._profilePictureUrl;
+            }
+            set => _profilePictureUrl = value;
+        }
+
         public string Address { get; set; }
         public string City { get; set; }
 
@@ -25,7 +40,7 @@ namespace KinMel.Models
                     string.IsNullOrWhiteSpace(this.FirstName) ? "" : this.FirstName;
                 string dspLastName =
                     string.IsNullOrWhiteSpace(this.LastName) ? "" : this.LastName;
-               
+
 
                 return $"{dspFirstName} {dspLastName}";
             }
@@ -69,7 +84,7 @@ namespace KinMel.Models
         public string ProfilePictureUrl { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        
+
         public virtual ICollection<ClassifiedAd> ClassifiedAds { get; set; }
 
         public virtual ICollection<Rating> Ratings { get; set; }
---
+++ KinMel/Models/ManageViewModels/ProfilePictureViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace KinMel.Models.ManageViewModels
+{
+    public class ProfilePictureViewModel
+    {
+
+        public string ProfilePictureUrl { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
