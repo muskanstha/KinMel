31c64f92e7e9898ef33423aa8d00469905583136
Vattujan <yoge0030@edu.easj.dk>
2018-05-15 13:10:17 +0200

    slider and some ui changes

--- KinMel/wwwroot/js/site.js
+++ KinMel/wwwroot/js/site.js
@@ -1,34 +1,37 @@
 ﻿// Write your JavaScript code.
+
 $(document).ready(function () {
     M.AutoInit();
+
     //preloader
     $(window).load(function () {
         setTimeout(function () {
             $("body").addClass("loaded");
         }, 150);
     });
+
     // Materialize Dropdown
     $('.dropdown-trigger').dropdown({
         coverTrigger: false // Displays dropdown below the button
     });
+
     //Tooltip
-    $(document).ready(function () {
-        $(".tooltipped").tooltip({ delay: 50 });
+    $(".tooltipped").tooltip({ delay: 50 });
 
         //var count = JSON.parse($.getJSON("notifications/notificationcount/"));
         //console.log(1);
         $.getJSON("notifications/notificationcount/",
             function (data) {
                 document.getElementById("notificationCount").innerHTML = data;
-            });
-    });
-    //$('.sidenav').sidenav();
+        });
 
-    //$('.parallax').parallax();
-
-
-    ////console.log(1);
-    //$('select').formSelect();
+    ////carousel
+    //$('.slider').slider({
+    //    fullWidth: true,
+    //    indicators: true,
+    //    duration: 800,
+    //    interval: 5000
+    //});
 
 
 });
